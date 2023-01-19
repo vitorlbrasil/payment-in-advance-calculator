30758f9edfe0ce7862be4b6538c6178f84b36675
Vitor Brasil <vitorlbrasil@hotmail.com>
2023-01-19 10:43:55 -0300

    refactor: increase space between form fields and button

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -3,7 +3,13 @@ import styled from "styled-components";
 export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 0.75rem;
+  gap: 1.5rem;
 
   width: 100%;
+
+  .formFieldsContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+  }
 `;
