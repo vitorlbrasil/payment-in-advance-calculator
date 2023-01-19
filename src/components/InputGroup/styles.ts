b73347a7d5ef1a03d13bb9386645c1db38e6c5e3
Vitor Brasil <vitorlbrasil@hotmail.com>
2023-01-19 20:03:34 -0300

    feat: add form schema and disable Input component

--- src/components/InputGroup/styles.ts
+++ src/components/InputGroup/styles.ts
@@ -6,4 +6,13 @@ export const StyledDiv = styled.div`
   gap: 0.25rem;
 
   width: 100%;
+
+  > input {
+    width: 100%;
+    padding: 0.25rem;
+    font-size: 0.875rem;
+    color: var(--grey-3);
+    border: 1px solid var(--grey-3);
+    border-radius: 0.25rem;
+  }
 `;
